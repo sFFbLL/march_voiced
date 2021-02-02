d1efa47a49e4173fae659a3431b3f19feb0f74a6
LiXiaoShan <2135958527@qq.com>
2021-02-02 22:55:58 +0800

    home head-nav 20210202

---
+++ march_voice_mobile/marchVoiceComponents/tabCard.vue
@@ -0,0 +1,60 @@
+<template>
+	<view>
+		<view class="head-nav">
+			<view class="head-nav-item" v-for="(item,index) in tabs" :key="item.index" :class="item.isActive ? 'active' : ''"
+			 @tap="handlerActive(item.index)">
+				<view class="center">{{item.value}}</view>
+			</view>
+		</view>
+		<slot></slot>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		},
+		props: {
+			tabs: {
+				type: Array,
+				default: []
+			}
+		},
+		methods: {
+			handlerActive(index) {
+				this.$emit('tabActive', index)
+			}
+		},
+		mounted() {}
+	}
+</script>
+
+<style lang="scss">
+	.head-nav {
+		display: -webkit-flex;
+		display: flex;
+
+		.head-nav-item {
+			flex: 1;
+			text-align: center;
+			font-size: 36rpx;
+			font-family: PingFangSC;
+			color: #999;
+		}
+	}
+
+	.head-nav-item .center {
+		margin: auto;
+		width: 40%;
+		height: 90%;
+	}
+
+
+	.active .center {
+		color: #404040;
+		border-bottom: 8rpx solid #404040;
+	}
+</style>
--- march_voice_mobile/pages/home/index.vue
+++ march_voice_mobile/pages/home/index.vue
@@ -1,13 +1,134 @@
 <template>
-
-	<view>
-
-		首页
+	<view class="home">
+		<view class="header">
+			<view class="head-nav-items">
+				<tabs :tabs='tablist' v-on:tabActive='tabActive' />
+			</view>
+			<view class="search">
+				<view class="search-icon">
+					<uni-icons type="search" size="30" color="#999"></uni-icons>
+				</view>
+			</view>
+		</view>
+		<view class="content">
+			<view v-for="(index,item) in articleList">
+				<!-- <recommend :article /> -->
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
+	import uniIcons from "@/components/uni-icons/uni-icons.vue"
+	import tabs from '../../marchVoiceComponents/tabCard.vue'
+	import recommend from '../../marchVoiceComponents/showArticle/recommend.vue'
+	export default {
+		components: {
+			uniIcons,
+			tabs,
+			recommend
+		},
+		data() {
+			return {
+				tablist: [{
+						index: 0,
+						value: '推荐',
+						isActive: true
+					},
+					{
+						index: 1,
+						value: '关注',
+						isActive: false
+					}
+				],
+				contentIndex: '',
+				articleList: [{
+					article: {
+						id: 1,
+						title: "文章1",
+						content: "文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容",
+						upDateTime: "更新时间",
+						favourTotal: 1,
+						collectTotal: 1,
+						commentTotal: 1
+					},
+					user: {
+						userId: 1,
+						userName: "张三",
+						userImage: "",
+						isFollow: "是否关注"
+					}
+				}, {
+					article: {
+						id: 1,
+						title: "文章1",
+						content: "文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容",
+						upDateTime: "更新时间",
+						favourTotal: 1,
+						collectTotal: 1,
+						commentTotal: 1
+					},
+					user: {
+						userId: 1,
+						userName: "张三",
+						userImage: "",
+						isFollow: "是否关注"
+					}
+				}]
+
+			}
+		},
+		methods: {
+			tabActive(tabIndex) {
+				this.tablist.map((value, index) => {
+					value.isActive = tabIndex == index ? true : false;
+				})
+				if (!tabIndex) {
+					this.recommend();
+				} else if (tabIndex) {
+					this.attention();
+				}
+				this.contentIndex = tabIndex;
+			},
+			recommend() {
+				console.log(1);
+			},
+			attention() {
+				console.log(2);
+			}
+		}
+	}
 </script>
 
 <style>
+	.home {
+		background-color: rgba(241, 241, 241, 1);
+	}
+
+	.header {
+		height: 100rpx;
+		line-height: 100rpx;
+		background-color: #fff;
+		margin-bottom: 15rpx;
+	}
+
+	.head-nav-items {
+		display: inline-block;
+		width: 90%;
+	}
+
+	.search {
+		display: inline-block;
+		width: 8%;
+	}
+
+	.search .search-icon {
+		position: absolute;
+		top: 5rpx;
+	}
+	
+	.content{
+		min-height: 300rpx;
+		background-color: #fff;
+	}
 </style>
