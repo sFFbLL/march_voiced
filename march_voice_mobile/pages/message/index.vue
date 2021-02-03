90dff2885a3d89d8b6ed2f827f0541932fd2b9e1
yanxin <1209189845@qq.com>
2021-02-03 22:44:50 +0800

    feat:yanxin day2

--- march_voice_mobile/pages/message/index.vue
+++ march_voice_mobile/pages/message/index.vue
@@ -1,12 +1,64 @@
 <template>
 	<view>
-		<view>
-			消息
+		<view class="header">
+			<view class="head-nav-items">
+				<tabs :tabs='tablist' v-on:tabActive='tabActive' />
+			</view>
+			
 		</view>
+		
 	</view>
 </template>
 
 <script>
+	import tabs from '../../marchVoiceComponents/tabCard.vue'
+	export default {
+		components: {
+			tabs
+		},
+		data() {
+			return {
+				tablist: [{
+						index: 0,
+						value: '互动',
+						isActive: true
+					},
+					{
+						index: 1,
+						value: '关注',
+						isActive: false
+					},
+					{
+						index: 2,
+						value: '其他',
+						isActive: false
+					}
+				],
+				contentIndex: '',
+			}
+		},
+		methods: {
+			tabActive(tabIndex) {
+				this.tablist.map((value, index) => {
+					value.isActive = tabIndex == index ? true : false;
+				})
+				if (!tabIndex) {
+					this.recommend();
+				} else if (tabIndex) {
+					this.attention();
+				}
+				this.contentIndex = tabIndex;
+			},
+			recommend() {
+				console.log(1);
+			},
+			attention() {
+				console.log(2);
+			}
+		},
+	}
+	
+	
 </script>
 
 <style>
