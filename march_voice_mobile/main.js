1c1ef4fb0e2d1306a687645a307e13741e764184
xianer-er <780248307@qq.com>
2021-02-03 22:58:53 +0800

    emjoi2/3

--- march_voice_mobile/main.js
+++ march_voice_mobile/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App'
 //引入vuex
 import store from './store'
+import uView from "uview-ui";
+Vue.use(uView);
 //把vuex定义成全局组件
 Vue.prototype.$store = store
 Vue.config.productionTip = false
@@ -9,8 +11,8 @@ Vue.config.productionTip = false
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App,
+	...App,
 	//挂载
-	    store
+	store
 })
 app.$mount()
