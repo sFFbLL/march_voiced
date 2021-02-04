42d00ae1b9f8dfe5c9c89b99b46b5566767baef2
LiXiaoShan <2135958527@qq.com>
2021-02-04 09:34:43 +0800

    fix:recommend components 0204

---
+++ march_voice_mobile/marchVoiceComponents/showArticle/childComponents/articleTitle.vue
@@ -0,0 +1,39 @@
+<template>
+	<!-- 文章标题 -->
+	<view class="article-title">{{articleTitle}}</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		},
+		props: {
+			articleTitle: {
+				type: String,
+				default: ""
+			}
+		},
+		components: {
+
+		},
+		methods: {
+
+		},
+		mounted() {
+
+		}
+	}
+</script>
+
+<style>
+	.article-title {
+		color: #404040;
+		font-size: 32rpx;
+		font-family: PingFangSC;
+		text-align: left;
+		font-weight: bold;
+	}
+</style>
--- march_voice_mobile/marchVoiceComponents/showArticle/recommend.vue
+++ march_voice_mobile/marchVoiceComponents/showArticle/recommend.vue
@@ -1,8 +1,60 @@
 <template>
+	<!-- 推荐页面文章展示组件 -->
+	<!-- 
+	 upDateTime: "更新时间",
+	 favourTotal: 1,
+	 collectTotal: 1,
+	 commentTotal: 1
+	 -->
+	<view class="recommend-component">
+		<!-- 文章标题 -->
+		<articleTitle :articleTitle="article.title" />
+		<!-- 文章内容 -->
+		<articleContent :articleContent="article.content"
+		 :articleImg="article.articleImg" />
+		<articleInteract :favourTotal="article.favourTotal"
+		 :commentTotal="article.commentTotal"
+		 :collectTotal="article.collectTotal"></articleInteract>
+	</view>
 </template>
 
 <script>
+	import articleTitle from "./childComponents/articleTitle.vue"
+	import articleContent from "./childComponents/artilceContent.vue"
+	import articleInteract from "./childComponents/articleInteract.vue"
+	export default {
+		data() {
+			return {
+				article: {},
+				userInfo: {}
+			};
+		},
+		props: {
+			articleInfo: {
+				type: Object,
+				default: {}
+			}
+		},
+		components: {
+			articleTitle,
+			articleContent,
+			articleInteract
+		},
+		methods: {
+
+		},
+		mounted() {
+			this.article = this.articleInfo.article;
+			this.user = this.articleInfo.userInfo;
+		}
+	}
 </script>
 
 <style>
+	.recommend-component {
+		background-color: #fff;
+		margin: auto;
+		margin-bottom: 15rpx;
+		padding: 30rpx 30rpx;
+	}
 </style>
