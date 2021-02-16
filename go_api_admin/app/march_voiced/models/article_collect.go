aed866b5c3f2d18e25018218df2b3a5f791101c2
linbolun <1668186101@qq.com>
2021-02-16 21:38:22 +0800

    creat article table struct

--- go_api_admin/app/march_voiced/models/article_collect.go
+++ go_api_admin/app/march_voiced/models/article_collect.go
@@ -1,1 +1,14 @@
 package models
+
+import "project/app/admin/models"
+
+type ArticleCollect struct {
+	ArticleId uint `json:"article_id" gorm:""`
+	CreatBy   uint `json:"creat_by" gorm:""`
+	UpdateBy  uint `json:"update_by" gorm:""`
+	models.BaseModel
+}
+
+func (a *ArticleCollect) TableName() string {
+	return `article_collect`
+}
--- go_api_admin/app/march_voiced/models/article_tag.go
+++ go_api_admin/app/march_voiced/models/article_tag.go
@@ -1,1 +1,15 @@
 package models
+
+import "project/app/admin/models"
+
+type ArticleTag struct {
+	Tag      string `json:"kind" gorm:""`
+	Icon     uint   `json:"icon" gorm:""`
+	CreatBy  uint   `json:"creat_by" gorm:""`
+	UpdateBy uint   `json:"update_by" gorm:""`
+	models.BaseModel
+}
+
+func (a *ArticleTag) TableName() string {
+	return `article_tag`
+}
