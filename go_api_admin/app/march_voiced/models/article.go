a80147f0fe8b53e88183292beb4cc5c0f7f08cc7
linbolun <1668186101@qq.com>
2021-02-18 16:47:40 +0800

    feat:article detail reprint top

--- go_api_admin/app/march_voiced/models/article.go
+++ go_api_admin/app/march_voiced/models/article.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"project/app/march_voiced/models/bo"
+	"project/app/march_voiced/models/dto"
 	"project/common/global"
 
 	"go.uber.org/zap"
@@ -13,8 +15,8 @@ type Article struct {
 	Tag         uint8  `json:"tag" gorm:"size:5;"`
 	Kind        uint8  `json:"kind" gorm:"size:4;"`
 	Status      *uint8 `json:"status" gorm:"size:2;"`
-	Type        *uint8 `json:"type" gorm:"size:1;"`
-	IsRecommend *uint8 `json:"is_recommend" gotm:"size:1;DEFAULT:0;"`
+	IsRecommend *uint8 `json:"is_recommend" gorm:"size:1;DEFAULT:0;"`
+	Type        uint   `json:"type" gorm:""`
 	CreatBy     uint   `json:"creat_by" gorm:""`
 	UpdateBy    uint   `json:"update_by" gorm:""`
 	BaseModel
@@ -25,22 +27,80 @@ func (a *Article) TableName() string {
 }
 
 func (a *Article) InsertArticle() (err error) {
-	err = global.Eloquent.Table(a.TableName()).Create(&a).Error
+	err = global.Eloquent.Table(a.TableName()).Create(a).Error
 	return
 }
 
 func (a *Article) UpdateArticle(id uint) (err error) {
 	update := new(Article)
 
-	err = global.Eloquent.Table(a.TableName()).Where("id = ?", id).First(&update).Error
+	err = global.Eloquent.Table(a.TableName()).Where("id = ? AND is_deleted = 0", id).First(update).Error
 	if err != nil {
-		zap.L().Error("UpdateArticle Dao Find First Failed")
+		zap.L().Error("UpdateArticle Dao Find First Failed", zap.Error(err))
 		return
 	}
 
-	err = global.Eloquent.Table(a.TableName()).Model(&update).Updates(&a).Error
+	err = global.Eloquent.Table(a.TableName()).Model(update).Updates(a).Error
 	if err != nil {
-		zap.L().Error("UpdateArticle Dao Update Failed")
+		zap.L().Error("UpdateArticle Dao Update Failed", zap.Error(err))
 	}
+	return
+}
+
+func (a *Article) ArticleDetail(id int, userId int) (userMsg bo.UserMsg, total [3]int64, err error) {
+	totalString := [3]string{"article_collect", "article_comment", "article_favour"}
+
+	// 获取文章信息
+	err = global.Eloquent.Table(a.TableName()).Where("id = ? AND is_deleted = 0", id).First(a).Error
+	if err != nil {
+		zap.L().Error("ArticleDetail Select article failed", zap.Error(err))
+		return
+	}
+
+	// 是转发的话
+	if a.Type != 0 {
+		err = global.Eloquent.Table(a.TableName()).Where("id = ? AND is_deleted = 0", a.Type).First(a).Error
+		if err != nil {
+			zap.L().Error("ArticleDetail Select type article failed", zap.Error(err))
+			return
+		}
+	}
+
+	// 获取用户信息
+	err = global.Eloquent.Table("sys_user").Select("id, nick_name, avatar_path").Where("id = ? AND is_deleted = 0", a.CreatBy).First(&userMsg).Error
+	if err != nil {
+		zap.L().Error("ArticleDetail Select articleUser failed", zap.Error(err))
+		return
+	}
+
+	// 是否关注
+	if uint(userId) != a.CreatBy {
+		var count int64
+		err = global.Eloquent.Table("follow").Where("follow_id = ? AND creat_by = ? AND is_deleted = 0", a.CreatBy, userId).Count(&count).Error
+		if err != nil {
+			zap.L().Error("ArticleDetail Select IsFollow failed", zap.Error(err))
+			return
+		}
+		if count > 0 {
+			userMsg.IsFollow = 1
+		}
+	}
+
+	// 获取文章total信息
+	for i, v := range totalString {
+		err = global.Eloquent.Table(v).Where("article_id = ? AND is_deleted = 0", id).Count(&total[i]).Error
+		if err != nil {
+			zap.L().Error("ArticleDetail Select total "+v+" failed", zap.Error(err))
+			return
+		}
+	}
+
+	return
+}
+
+func (a *Article) TopArticleList(paging dto.Paging) (articleArray *[]Article, err error) {
+	articleArray = new([]Article)
+	err = global.Eloquent.Table(a.TableName()).Where("is_deleted = 0 AND is_recommend = 1 AND type = 0 AND status = 1").
+		Limit(paging.Size).Offset((paging.Current - 1) * paging.Size).Find(articleArray).Error
 	return
 }
--- go_api_admin/app/march_voiced/router/article.go
+++ go_api_admin/app/march_voiced/router/article.go
@@ -2,35 +2,23 @@ package router
 
 import (
 	"project/app/march_voiced/apis"
-	"project/utils/app"
 
 	"github.com/gin-gonic/gin"
 )
 
 func init() {
-	routerNoCheckRole = append(routerNoCheckRole, articleRouter)
 	routerCheckRole = append(routerCheckRole, articleAuthRouter)
-}
-
-// 无需认证的路由代码
-func articleRouter(v1 *gin.RouterGroup) {
-	r := v1.Group("/article")
-	{
-		r.GET("ping", func(c *gin.Context) {
-			c.String(int(app.CodeSuccess), "ok")
-		})
-	}
 }
 
 // 需认证的路由代码
 func articleAuthRouter(v1 *gin.RouterGroup) {
 	r := v1.Group("/article")
 	{
-		//r.GET("/:id")
+		r.GET("detail/:id", apis.ArticleDetailHandler)
 		r.POST("", apis.InsertArticleHandler)
 		r.PUT("", apis.UpdateArticleHandler)
-		//r.DELETE("/:id")
-		//r.GET("/")
-		//r.POST("/superior")
+		r.POST("reprint", apis.ReprintArticleHandler)
+		r.GET("top", apis.TopArticleListHandler)
 	}
+
 }
