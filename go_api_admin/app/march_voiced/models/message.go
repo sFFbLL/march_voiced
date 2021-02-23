70944e28211a2687da7ae5d0aa815031eb48062c
山鸡 <1570354680@qq.com>
2021-02-23 14:57:58 +0800

    fix:article message for follow

--- go_api_admin/app/march_voiced/models/message.go
+++ go_api_admin/app/march_voiced/models/message.go
@@ -47,7 +47,7 @@ func (e *Message) GetMessage(getMessage *bo.GetMessage, p *dto.Paginator, userId
 	err = global.Eloquent.Table(e.TableName()).
 		Select("sys_user.avatar_path, sys_user.nick_name, message.type , message.status, message.article_id, message.create_by, message.create_time, message.title, message.comment, message.content, message.image").
 		Joins("left join sys_user on sys_user.id = message.create_by").
-		Where("message.create_by in (?) and sys_user.is_deleted=0", followList).Count(&getMessage.Total).
+		Where("message.type=0 and message.create_by in (?) and sys_user.is_deleted=0", followList).Count(&getMessage.Total).
 		Order("message.create_time desc").Limit(int(p.Size)).Offset(int(p.Current - 1*p.Size)).
 		Find(getMessage.Records).Error
 	return
