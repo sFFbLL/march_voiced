ff9d13870ea19189c743460c2d59c288196ca16a
xianer-er <780248307@qq.com>
2021-03-10 10:46:57 +0800

    testingLogin3/10

--- march_voice_mobile/pages/pastArticle/index.vue
+++ march_voice_mobile/pages/pastArticle/index.vue
@@ -2,13 +2,43 @@
 	<view class="wrap">
 		<text class="title">欢迎登录三月之声</text>
 		<view class="inputs">
-			<input class="input" placeholder="请输入简书链接" />
-			<input class="input" placeholder="请输入CSDN链接" /></view>
-
+			<input class="input" placeholder="请输入简书链接(必填项)"   @input="janInput"/>
+			<input class="input" placeholder="请输入CSDN链接"  @input="csdnInput"/></view>
+				<button :loading="btnloading" class="submitbtn" @click="toSubmit">提交</button>
 	</view>
 </template>
 
 <script>
+	export default{
+		data() {
+			return {
+				btnloading: false,
+				janValue:"",
+				csdnValue:"",
+			}
+		},
+		methods:{
+			// 获取链接
+			janInput(event) {
+				this.janValue = event.target.value;
+			},
+			csdnInput(event) {
+				this.csdnValue = event.target.value;
+			},
+			toSubmit(){
+				if(this.janValue==""){
+					uni.showToast({
+						title: '请完善简书链接',
+						icon: "none",
+						position: "top",
+						duration: 2000
+					});
+				}else{
+					// 调用接口
+				}
+			}
+		}
+	}
 </script>
 
 <style scoped>
@@ -27,4 +57,12 @@
 		width: 600rpx;
 		border-bottom: 2rpx solid #f1eeec;
 	}
+	.submitbtn {
+		width: 300rpx;
+		margin-top: 250rpx;
+		border-radius: 40rpx;
+		color: #FFFFFF;
+		background-image: linear-gradient(140deg, #7ce1e6 0%, #5b97e5 100%);
+	}
+	
 </style>
